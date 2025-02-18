054fe9c65aaf65419170a75c1db8cb63548ea052
Abhimanyu B <bijuabhimanyu15@gmail.comgit config --global user.name abhi>
2025-02-18 11:44:20 +0530

    final commit

---
+++ contactform/contact_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+from . models import Contact
+from .forms import ContactForm
+from django.contrib import messages
+
+# Create your views here.
+
+def contact(request):
+    form = ContactForm()  
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Form Successfully Submitted')
+            return redirect('contact')
+    return render(request, "contact.html", {"form": form})
